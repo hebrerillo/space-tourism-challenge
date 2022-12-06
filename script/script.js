236ad4227052a0b28105893c553a1e2f8a001ac5
hebrerillo <hebrerillo@gmail.com>
2022-12-06 12:13:09 +0100

    Implementing a nice transition to show elements

--- script/script.js
+++ script/script.js
@@ -2,6 +2,24 @@
 
 const tabContainer = document.querySelector('[data-tabContainer]');
 const targetsContainers = document.querySelectorAll('[data-targets-container]');
+const allTargets = document.querySelectorAll('.target-tab');
+
+allTargets.forEach(target => 
+{
+    target.addEventListener('transitionend', function()
+    {
+        if (this.classList.contains('hiding-target-tab'))
+        {
+            this.classList.add('hidden-target-tab');
+            const nextTarget = this.parentNode.querySelector('[data-source-target="' + this.dataset.targetRole + '"]');
+            nextTarget.style.transition = 'none';
+            nextTarget.classList.remove('hidden-target-tab');
+            nextTarget.style.transition = '';
+            nextTarget.offsetHeight;
+            nextTarget.classList.remove('hiding-target-tab');
+        }
+    });
+});
 
 const handleClickedTab = function(event){
     const clickedTab = event.target.closest('[data-tab]');
@@ -22,11 +40,17 @@ const showTarget = function(targetElement) {
        targetContainer.querySelectorAll('[data-source-target]').forEach(source => {
            if(source.dataset.sourceTarget === targetElement)
            {
-               source.classList.remove('hidden-target-tab');
+//               source.dataset.targetRole = 'next';
+//               source.style.transition = 'none';
+//               source.classList.remove('hidden-target-tab');
+//               source.style.transition = '';
+//               source.offsetHeight;
+//               source.classList.remove('hiding-target-tab');
            }
-           else
+           else if (!source.classList.contains('hiding-target-tab'))
            {
-               source.classList.add('hidden-target-tab');
+               source.dataset.targetRole = targetElement;
+               source.classList.add('hiding-target-tab');
            }
        }); 
     });
